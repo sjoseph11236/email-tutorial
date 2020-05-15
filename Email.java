7216e853eac40a4910153f6ce4a98cf02349f750
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-05-15 17:18:58 -0400

    chore: working on setDepartment

--- Email.java
+++ Email.java
@@ -10,10 +10,15 @@ public class Email {
   public Email(String firstName, String lastName){
     this.firstName = firstName;
     this.lastName = lastName;
+    
+    // call a mthod asking for the department - return the deparment
+
   }
 
   // Ask for the department
-
+  private String setDeparment( ) {
+    System.out.print("Enter the department\n1 for Sales\n2 for Accounting\n3 for Development\n0 for none");
+  }
   // Set the mailbox capcity
   
   // Set the alternate email
---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main { 
+
+  public static void main(String[] args) {
+     Email email = new Email("John" , "Doe");
+  } 
+}
