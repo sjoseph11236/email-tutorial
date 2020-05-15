4f376824e91ede96490fdb8bb9a25970c7406f50
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-05-15 17:44:09 -0400

    chore: add the department method

--- Email.java
+++ Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
   private String firstName; 
   private String lastName;
@@ -10,14 +12,24 @@ public class Email {
   public Email(String firstName, String lastName){
     this.firstName = firstName;
     this.lastName = lastName;
-    
-    // call a mthod asking for the department - return the deparment
 
+    // call a mthod asking for the department - return the deparment
+    this.department = setDeparment();
+    System.out.println("Department: " + this.department);
   }
 
   // Ask for the department
   private String setDeparment( ) {
-    System.out.print("Enter the department\n1 for Sales\n2 for Accounting\n3 for Development\n0 for none");
+    System.out.print("DEPARTMENT CODES\nEnter the department\n1 for Sales\n2 for Accounting\n3 for Development\n0 for none\nEnter department code: ");
+    Scanner in = new Scanner(System.in);
+    // Scans the entry as an integer
+    int departmentChoice = in.nextInt();
+
+    if(departmentChoice == 1) {return "Sales"; }
+    else if (departmentChoice == 2){ return "Accounting"; }
+    else if (departmentChoice == 3) { return "Development"; }
+    else { return ""; }
+    
   }
   // Set the mailbox capcity
   
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main { 
 
   public static void main(String[] args) {
-     Email email = new Email("John" , "Doe");
+    Email email = new Email("John" , "Doe");
   } 
 }
