d17ee762eda9a2b0b2e073358104930ed86de6b8
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-05-15 17:09:32 -0400

    chore: add constructor and Readme

--- Email.java
+++ Email.java
@@ -1,4 +1,23 @@
 public class Email {
-  String firstName; 
-  String lastName;
+  private String firstName; 
+  private String lastName;
+  private String password; 
+  private String department;
+  private int mainlboxCapcity; 
+  private String alternateEmail; 
+
+  // constructor to receive the firstName and lastName
+  public Email(String firstName, String lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  // Ask for the department
+
+  // Set the mailbox capcity
+  
+  // Set the alternate email
+
+  // Change the password
+
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+# Email Application 
+
+- Generate an email with the following syntax: firstname.lastname@department.company.com
+- Determine the department(Sales, development, accounting) if none leave blank 
+- Generate a random String for a password. 
+- Have set methods to: 
+  - change the password.
+  - set the mailbox capacity. 
+  - define alternate email address. 
+- Have get methods to display the name, email and mailbox capacity.
